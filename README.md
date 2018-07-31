7a55c71d675a4c64e1a9465fbd6d983c2e1c72ce
Julian Paul Dasmarinas <julian.dasma@gmail.com>
2018-07-31 14:12:32 +0800

    Added files

---
+++ README.md
@@ -0,0 +1,11 @@
+# exec-all
+
+## Overview
+
+Run a command in all of the containers in a specific resource.
+
+## Usage
+
+```
+kubectl plugin exec-as $RESOURCE_TYPE $RESOURCE_NAME -- $COMMAND
+```
---
+++ exec-all/exec-all.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if which jq &> /dev/null; then
+    echo "Please install jq" 2>&1
+    exit 1
+fi
+
+RESOURCE="${1}"
+shift
+RESOURCE_NAME="${1}"
+shift
+
+KUBECTL=${KUBECTL_PLUGINS_CALLER}
+NAMESPACE=${KUBECTL_PLUGINS_GLOBAL_FLAG_NAMESPACE:-default}
+
+CONTAINER=${KUBECTL_PLUGINS_LOCAL_FLAG_CONTAINER:-}
+PARALLEL=${KUBECTL_PLUGINS_LOCAL_FLAG_PARALLEL:-1}
+
+
+selector=$($KUBECTL -n "$NAMESPACE" get "$RESOURCE" "$RESOURCE_NAME" -o json |  jq -j '.spec.selector.matchLabels | to_entries | .[] | "\(.key)=\(.value),"')
+selector=${selector%,}
+
+if [[ -z "$CONTAINER" ]]; then
+    $KUBECTL get pods -n "$NAMESPACE" --selector="$selector"  | sed '1 d' | gawk '{ print $1 }' | xargs -I{} -n1 -P"${PARALLEL}" "$KUBECTL" exec -n "$NAMESPACE" -it '{}' -- "$@"
+else
+    $KUBECTL get pods -n "$NAMESPACE" --selector="$selector"  | sed '1 d' | gawk '{ print $1 }' | xargs -I{} -n1 -P"${PARALLEL}" "$KUBECTL" exec -n "$NAMESPACE" -it --container="${CONTAINER}" '{}' -- "$@"
+fi
