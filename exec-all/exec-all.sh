bef25e281b5f7cf56e2b7a381aa50413f69bd7fd
Julian Paul Dasmarinas <JulianPaul.Dasmarinas@bayviewtechnology.com>
2018-08-06 13:13:48 +0800

    Take into account if kubeconfig is specified

--- exec-all/exec-all.sh
+++ exec-all/exec-all.sh
@@ -13,16 +13,20 @@ shift
 
 KUBECTL=${KUBECTL_PLUGINS_CALLER}
 NAMESPACE=${KUBECTL_PLUGINS_GLOBAL_FLAG_NAMESPACE:-default}
+KUBECONFIG=${KUBECTL_PLUGINS_GLOBAL_FLAG_KUBECONFIG:-${HOME}/.kube/config}
 
-CONTAINER=${KUBECTL_PLUGINS_LOCAL_FLAG_CONTAINER:-}
+if [[ -z ${KUBECTL_PLUGINS_LOCAL_FLAG_CONTAINER} ]]; then
+    CONTAINER_FLAG=
+else
+    CONTAINER_FLAG="--container=${KUBECTL_PLUGINS_LOCAL_FLAG_CONTAINER}"
+fi
+
 PARALLEL=${KUBECTL_PLUGINS_LOCAL_FLAG_PARALLEL:-1}
 
 
-selector=$($KUBECTL -n "$NAMESPACE" get "$RESOURCE" "$RESOURCE_NAME" -o json |  jq -j '.spec.selector.matchLabels | to_entries | .[] | "\(.key)=\(.value),"')
+selector=$($KUBECTL --kubeconfig="${KUBECONFIG}" -n "$NAMESPACE" get "$RESOURCE" "$RESOURCE_NAME" -o json |  jq -j '.spec.selector.matchLabels | to_entries | .[] | "\(.key)=\(.value),"')
 selector=${selector%,}
 
-if [[ -z "$CONTAINER" ]]; then
-    $KUBECTL get pods -n "$NAMESPACE" --selector="$selector"  -o json | jq '.items | .[].metadata.name ' | xargs -I{} -n1 -P"${PARALLEL}" "$KUBECTL" exec -n "$NAMESPACE" -it '{}' -- "$@"
-else
-    $KUBECTL get pods -n "$NAMESPACE" --selector="$selector"  -o json | jq '.items | .[].metadata.name ' | xargs -I{} -n1 -P"${PARALLEL}" "$KUBECTL" exec -n "$NAMESPACE" -it --container="${CONTAINER}" '{}' -- "$@"
-fi
+$KUBECTL --kubeconfig="${KUBECONFIG}" get pods -n "$NAMESPACE" --selector="$selector"  -o json \
+    | jq '.items | .[].metadata.name ' \
+    | xargs -I{} -n1 -P"${PARALLEL}" "$KUBECTL" --kubeconfig="${KUBECONFIG}" exec -n "$NAMESPACE" -it "$CONTAINER_FLAG" '{}' -- "$@"
