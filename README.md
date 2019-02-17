777feec0f3e0b2629543ed7d0de114a8a1991a73
Julian Paul Dasmarinas <julian.dasma@gmail.com>
2019-02-17 17:36:12 +0800

    Added flag for custom selector

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ Run a command in all of the containers in a specific resource.
 ## Usage
 
 ```
-kubectl exec_all [-c container_name] [-p number_parallel_executions] [-n namespace] resource resource_name command
+kubectl exec-all [-c container_name] [-p number_parallel_executions] [-n namespace] resource resource_name command
 ```
 
 ## Examples
@@ -19,5 +19,11 @@ kubectl exec_all [-c container_name] [-p number_parallel_executions] [-n namespa
 Execute "docker system prune -a -f" in all of the containers in a daemonset named docker-daemon (with mounted docker.sock)
 
 ```
-kubectl plugin exec-all daemonset docker-daemon -- docker system prune -a -f
+kubectl exec-all daemonset docker-daemon docker system prune -a -f
+```
+
+Check a specific log for all the container specified with selector.
+
+```
+kubectl exec-all -s "label=app" cat /var/log/test.log
 ```
--- unix/exec-all.sh
+++ unix/exec-all.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -eo pipefail
+unalias kubectl
+kubectl=$(which kubectl)
 
 if ! which jq &> /dev/null; then
     echo "Please install jq" 1>&2
@@ -8,7 +10,8 @@ fi
 
 print_usage() {
     cat << EOF
-Usage: kubectl exec_all [-c container_name] [-p number_parallel_executions] [-n namespace] resource resource_name command
+Usage: kubectl exec-all [-c container_name] [-p number_parallel_executions] [-n namespace] resource resource_name command
+       kubectl exec-all [-c container_name] [-p number_parallel_executions] [-n namespace] -s selector command
 
 Options:
 -c          Container name if there are multiple containers in the pod. If not included, it will use the default container.
@@ -21,6 +24,7 @@ exit 1
 container=
 parallel=1
 namespace=
+custom_selector=
 while true; do
     case "$1" in
         "-c")
@@ -38,24 +42,15 @@ while true; do
             namespace="$1"
             shift
             ;;
+        "-s")
+            shift
+            custom_selector="$1"
+            shift
         *)
             break
             ;;
     esac
 done
-
-set -u
-
-resource="$1"
-resource_name="$2"
-
-if [[ -z "${resource}" || -z "${resource_name}" ]]; then
-    print_usage
-fi
-
-shift;shift;
-
-kubectl=$(which kubectl)
 
 if [[ -z ${container} ]]; then
     container_flag=
@@ -69,9 +64,22 @@ else
     namespace_flag="--namespace=${namespace}"
 fi
 
-# shellcheck disable=SC2086
-selector=$(${kubectl} ${namespace_flag} get "$resource" "$resource_name" -o json |  jq -j '.spec.selector.matchLabels | to_entries | .[] | "\(.key)=\(.value),"')
-selector=${selector%,}
+
+if [[ -z "$custom_selector" ]]; then
+    selector=${custom_selector}
+else
+    resource="$1"
+    resource_name="$2"
+    
+    if [[ -z "${resource}" || -z "${resource_name}" ]]; then
+        print_usage
+    fi
+    
+    shift;shift;
+    # shellcheck disable=SC2086
+    selector=$(${kubectl} ${namespace_flag} get "$resource" "$resource_name" -o json |  jq -j '.spec.selector.matchLabels | to_entries | .[] | "\(.key)=\(.value),"')
+    selector=${selector%,}
+fi
 
 # shellcheck disable=SC2086
 ${kubectl} get pods ${namespace_flag} --selector="$selector"  -o json | jq '.items | .[].metadata.name ' | xargs -I{} -n1 -P"${parallel}" "${kubectl}" exec $namespace_flag $container_flag '{}' -- "$@"
